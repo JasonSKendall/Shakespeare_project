a390a6560410e44689fb71dab23e1c548c02118c
Jason Kendall <jasonskendall@gmail.com>
2023-09-19 17:54:22 -0400

    refactoring

--- gather_data.py
+++ gather_data.py
@@ -3,29 +3,23 @@
 import re
 import urllib.request, urllib.parse, urllib.error
 
+location_of_source = 'mac'
+play = 'romeo_juliet'
 
-list_of_plays = [ 'othello', 'much_ado', 'lear', 'midsummer', 'merry_wives', 'twelfth_night', 'titus',
+def choose_play(loc, play):
+  list_of_plays = [ 'othello', 'much_ado', 'lear', 'midsummer', 'merry_wives', 'twelfth_night', 'titus',
                  'allswell', 'asyoulikeit', 'cymbeline', 'lll', 'measure', 'merchant', 'pericles', 'taming_shrew',
                  'tempest', 'troilus_cressida', 'two_gentlemen', 'winters_tale', '1henryiv', '2henryiv', 'henryv',
                  '1henryvi', '2henryvi', '3henryvi', 'henryviii', 'john', 'richardii', 'richardiii', 'cleopatra',
                  'coriolanus', 'hamlet', 'julius_caesar', 'macbeth', 'romeo_juliet', 'timon', 'comedy_errors' ]
-
-
-play = list_of_plays[17]
-print(play)
-
-
-
-
-url_to_grab = 'http://shakespeare.mit.edu/' + play + '/full.html'
-fhand = urllib.request.urlopen(url_to_grab)
-
-play = 'julius_caesar'
-fhand = open('/Users/jasonkendall/Desktop/shakespeare/' + play + '/full.html')
-
-casting = dict()
-list_of_scenes = []
-list_of_roles = []
+  if play == None:
+    play = list_of_plays[17]
+  print(play)
+  if loc == 'web':
+    url_to_grab = 'http://shakespeare.mit.edu/' + play + '/full.html'
+    return urllib.request.urlopen(url_to_grab)
+  else:
+    return open('/Users/jasonkendall/Desktop/shakespeare/' + play + '/full.html')
 
 def read_in_play_data():
 
@@ -33,8 +27,10 @@ def read_in_play_data():
   thescene = 'Prologue'
 
   for rawline in fhand:
-    # j = rawline.decode().rstrip()
-    j = rawline.rstrip()
+    if location_of_source == 'web':
+      j = rawline.decode().rstrip()
+    else:
+      j = rawline.rstrip()
 
     Divider_act = "H3>ACT "
     isact = j.find(Divider_act)
@@ -55,7 +51,6 @@ def read_in_play_data():
     if isscene_prologue != -1:
       act_and_scene = theact + '.Prologue'  
       list_of_scenes.append(act_and_scene)
-      print(act_and_scene)
       continue
 
     Divider_role = "NAME=speech"
@@ -70,9 +65,6 @@ def read_in_play_data():
       else:
         casting[key_role] = 1
 
-
-
-
 def fix_role_name(i):
   role_fixes = { 'First_': '_1', 'Second_': '_2', 'Third_': '_3', 'Fourth_': '_4'}
   for j in role_fixes.keys():
@@ -80,35 +72,40 @@ def fix_role_name(i):
       i = i.replace(j, '') + role_fixes[j]
   return i
 
+def create_role_scene_list():
+  for i in list_of_scenes:
+    scenelist.append(i)
+  breakdown_data['Role'] = scenelist
 
+def create_list_of_scenes_per_role():
+  for i in list_of_roles:
+    scenelist = []
+    for j in list_of_scenes:
+      key = (i,j)
+      if key in casting:
+        speech_counts = casting[key]
+      else:
+        speech_counts = 0
+      scenelist.append(speech_counts)
+    breakdown[i] = scenelist
+  
+def print_out_breakdown():
+  print('Role' , ' : ' ,breakdown_data['Role'])
+  for key in sorted(breakdown.keys()):
+    print(key, ' : ' , breakdown[key] )
 
-
-read_in_play_data()
-
-breakdown_2_data = {}
-breakdown_2 = {}
+fhand = choose_play(location_of_source, play)
+casting = dict()
+list_of_scenes = []
+list_of_roles = []
+breakdown_data = {}
+breakdown = {}
 scenelist = []
 
-for i in list_of_scenes:
-  scenelist.append(i)
-breakdown_2_data['Role'] = scenelist
-
-for i in list_of_roles:
-  scenelist = []
-  for j in list_of_scenes:
-    key = (i,j)
-    if key in casting:
-      speech_counts = casting[key]
-    else:
-      speech_counts = 0
-    scenelist.append(speech_counts)
-  breakdown_2[i] = scenelist
-  
+read_in_play_data()
+create_role_scene_list()
+create_list_of_scenes_per_role()
+print_out_breakdown()
 
 
 
-
-
-print('Role' , ' : ' ,breakdown_2_data['Role'])
-for key in sorted(breakdown_2.keys()):
-  print(key, ' : ' , breakdown_2[key] )
