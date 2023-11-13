a3be948c436bdb473811462d21510b6f9706681f
Jason Kendall <jasonskendall@gmail.com>
2023-11-13 13:25:28 -0500

    put all in the class

--- gather_data.py
+++ gather_data.py
@@ -4,8 +4,9 @@ import re
 import urllib.request, urllib.parse, urllib.error
 
 class Breakdown:
-  def __init__(self, play=None, list_of_scenes=[], list_of_roles = [], casting={}, breakdown={}) -> None:
+  def __init__(self, play=None, loc=None, list_of_scenes=[], list_of_roles = [], casting={}, breakdown={}) -> None:
     self.play = play
+    self.loc = loc
     self.list_of_scenes = list_of_scenes
     self.list_of_roles = list_of_roles
     self.casting = casting
@@ -67,8 +68,65 @@ class Breakdown:
   def print_out_breakdown(self):
     self.create_list_of_scenes_per_role()
     print('Role' , ' : ' ,self.list_of_scenes)
-    for key in sorted(p1.breakdown.keys()):
+    for key in sorted(self.breakdown.keys()):
       print(key, ' : ' , self.breakdown[key] )
+
+  def choose_play(self):
+    if self.loc == 'web':
+      # url_to_grab = 'http://shakespeare.mit.edu/' + play + '/full.html'
+      url_to_grab = 'http://www.jasonkendall.com/shakespeare_plays/' + self.play + '.html'
+      return urllib.request.urlopen(url_to_grab)
+    else:
+      return open('/Users/jasonkendall/Desktop/shakespeare/' + self.play + '/full.html', mode='r')
+
+
+  def fix_role_name(self, rolename=None):
+    self.rolename = rolename
+    role_fixes = { 'First_': '_1', 'Second_': '_2', 'Third_': '_3', 'Fourth_': '_4' , 'Fifth_': '_5' , 'Sixth_': '_6' , 'Seventh_': '_7' }
+    for j in role_fixes.keys():
+      if rolename.find(j, 0, ) != -1:
+        rolename = rolename.replace(j, '') + role_fixes[j]
+    return rolename
+
+  
+  def read_in_play_data(self):
+    fhand = self.choose_play()
+    theact = 'Induction'
+    thescene = 'Prologue'
+    for rawline in fhand:
+      if self.loc == 'web':
+        j = rawline.decode().rstrip()
+      else:
+        j = rawline.rstrip()
+      Divider_act = "H3>ACT "
+      isact = j.find(Divider_act)
+      if isact != -1:
+        theact = re.split('[<> ]',j)[3]
+        continue
+      Divider_scene = "h3>SCENE "
+      isscene = j.find(Divider_scene)
+      if isscene != -1:
+        thescene= re.split('[<> \.]',j)[3]
+        act_and_scene = theact + '.' + thescene
+        self.list_of_scenes.append(act_and_scene)
+        continue
+      Divider_scene_prologue = "h3>PROLOGUE" 
+      isscene_prologue = j.find(Divider_scene_prologue)
+      if isscene_prologue != -1:
+        act_and_scene = theact + '.Prologue'  
+        self.list_of_scenes.append(act_and_scene)
+        continue
+      Divider_role = "NAME=speech"
+      isrole = j.find(Divider_role) 
+      if isrole != -1: 
+        role = self.fix_role_name(  re.sub( ' ' , '_' ,   re.split('[\<|\>]', j)[4]  ) )
+        if role not in self.list_of_roles:
+          self.list_of_roles.append(role)
+        key_role = (role, act_and_scene)
+        if key_role in self.casting:
+          self.casting[key_role] += 1
+        else:
+          self.casting[key_role] = 1
 
   def print_out_breakdown_html(self):
     self.create_list_of_scenes_per_role()
@@ -83,93 +141,20 @@ class Breakdown:
       curline += i
     curline += '</TR>'
     print(curline)
-
-    for key in sorted(p1.breakdown.keys()):
+    for key in sorted(self.breakdown.keys()):
       curline = "<TR><TD>" + key + "</TD>"
       for i in self.breakdown[key]:
         curline += '<TD>' + str(i) + '</TD>'
       curline += '</TR>'
       print(curline)
-    
     print('</table>')
 
-def choose_play(loc, play):
-  if play == None:
-    play = p1.play
-  print(play)
-  if loc == 'web':
-    # url_to_grab = 'http://shakespeare.mit.edu/' + play + '/full.html'
-    url_to_grab = 'http://www.jasonkendall.com/shakespeare_plays/' + play + '.html'
-
-    return urllib.request.urlopen(url_to_grab)
-  else:
-    return open('/Users/jasonkendall/Desktop/shakespeare/' + play + '/full.html', mode='r')
-
-def read_in_play_data(loc, play):
-
-  fhand = choose_play(loc, play)
-
-  theact = 'Induction'
-  thescene = 'Prologue'
-
-  for rawline in fhand:
-    if location_of_source == 'web':
-      j = rawline.decode().rstrip()
-    else:
-      j = rawline.rstrip()
-
-    Divider_act = "H3>ACT "
-    isact = j.find(Divider_act)
-    if isact != -1:
-      theact = re.split('[<> ]',j)[3]
-      continue
-
-    Divider_scene = "h3>SCENE "
-    isscene = j.find(Divider_scene)
-    if isscene != -1:
-      thescene= re.split('[<> \.]',j)[3]
-      act_and_scene = theact + '.' + thescene
-      p1.list_of_scenes.append(act_and_scene)
-      continue
-
-    Divider_scene_prologue = "h3>PROLOGUE" 
-    isscene_prologue = j.find(Divider_scene_prologue)
-    if isscene_prologue != -1:
-      act_and_scene = theact + '.Prologue'  
-      p1.list_of_scenes.append(act_and_scene)
-      continue
-
-    Divider_role = "NAME=speech"
-    isrole = j.find(Divider_role) 
-    if isrole != -1: 
-      role = fix_role_name (  re.sub( ' ' , '_' ,   re.split('[\<|\>]', j)[4]  ) )
-      if role not in p1.list_of_roles:
-        p1.list_of_roles.append(role)
-      key_role = (role, act_and_scene)
-      if key_role in p1.casting:
-        p1.casting[key_role] += 1
-      else:
-        p1.casting[key_role] = 1
-
-def fix_role_name(i):
-  role_fixes = { 'First_': '_1', 'Second_': '_2', 'Third_': '_3', 'Fourth_': '_4' , 'Fifth_': '_5' , 'Sixth_': '_6' , 'Seventh_': '_7' }
-  for j in role_fixes.keys():
-    if i.find(j, 0, ) != -1:
-      i = i.replace(j, '') + role_fixes[j]
-  return i
 
 
 
 
 
-p1 = Breakdown()
-# play = p1.play
-  
-play = "midsummer"
-#location_of_source = 'mac'
-location_of_source = 'web'
 
-
-read_in_play_data(location_of_source, play)
+p1 = Breakdown("midsummer", "web")
+p1.read_in_play_data()
 p1.print_out_breakdown_html()
-
